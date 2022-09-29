fc72959d0f5b028ee07e99bd1d2e57c950c7e444
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-09-29 14:34:48 +0200

    Preparation for the next step

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -38,24 +39,27 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
+
   let content = <p>Found no movies.</p>;
 
-  if (movies.length) {
-    content = (
-      <p>
-        <MoviesList movies={movies} />
-      </p>
-    );
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
   if (error) {
     content = <p>{error}</p>;
   }
   if (isLoading) {
-    content = <p>Loading ...</p>;
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
